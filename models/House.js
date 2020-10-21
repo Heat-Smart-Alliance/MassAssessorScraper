7e166d710d747791f95f8877de160a3a72cd20e0
dodellmitchell@gmail.com <dodellmitchell@gmail.com>
2020-10-21 11:15:10 -0700

    Added more fields and refactored scraper to prevent timeout.

--- models/House.js
+++ models/House.js
@@ -18,6 +18,56 @@ const houseSchema = new Schema({
         type: String,
         required: false,
         set: deleteEmpty
+    },
+    address: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    stories: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    grade: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    model: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    owner: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    assessment: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    salePrice: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    yearBuilt: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    occupancy: {
+        type: String,
+        required: false,
+        set: deleteEmpty
+    },
+    totalRooms: {
+        type: String,
+        required: false,
+        set: deleteEmpty
     }
 });
 
--- src/index.js
+++ src/index.js
@@ -6,75 +6,108 @@ const House = require('../models/House');
 (async () => {
     const cluster = await Cluster.launch({
         concurrency: Cluster.CONCURRENCY_CONTEXT,
-        maxConcurrency: 15,
+        maxConcurrency: 30,
         monitor: true
     });
+
+    cluster.on('taskerror', (err, data, willRetry) => {
+        if (willRetry) {
+            console.warn(`Encountered an error while crawling ${data}. ${err.message}\nThis job will be retried`);
+        } else {
+            console.error(`Failed to crawl ${data}: ${err.message}`);
+        }
+    });
+
+
 
     const saveHouse = async ({page, data: url }) => {
         await page.goto(url);
         const houseData = await page.evaluate(async () => {
+            function getTableElement(table, ...searchTerms){
+                const elementText = table.filter(htmlRow => {
+                    const actualRow = [...htmlRow.getElementsByTagName('td')]
+                    const tableLabel = actualRow[0];
+                    const containsTerm = tableLabel && searchTerms.map(searchTerm => tableLabel.innerText.toLowerCase().includes(searchTerm)).some(label => label);
+                    return containsTerm
+                });
+                return elementText && elementText[0].getElementsByTagName("td")[1].innerText;
+            }
+            const table = Array.from(document.getElementById("MainContent_ctl01_grdCns").rows);
             return {
-                heatType: Array.from(Array.from(document.querySelectorAll(".RowStyle, .AltRowStyle")).filter(row => row.querySelector("td").innerText.includes("Heat Type") || row.querySelector("td").innerText.includes("Heating Type"))[0].querySelectorAll("td"))[1].innerText,
-                heatFuel: Array.from(Array.from(document.querySelectorAll(".RowStyle, .AltRowStyle")).filter(row => row.querySelector("td").innerText.includes("Heat Fuel") || row.querySelector("td").innerText.includes("Heating Fuel"))[0].querySelectorAll("td"))[1].innerText
+                owner: document.getElementById("MainContent_lblGenOwner").innerText,
+                assessment: document.getElementById("MainContent_lblGenAssessment").innerText,
+                salePrice: document.getElementById("MainContent_lblPrice").innerText,
+                saleDate: document.getElementById("MainContent_lblSaleDate").innerText,
+                yearBuilt: document.getElementById("MainContent_ctl01_lblYearBuilt").innerText,
+                occupancy: getTableElement(table, "occupancy"),
+                totalRooms: getTableElement(table, "total rooms"),
+                style: getTableElement(table, "style"),
+                model: getTableElement(table, "model"),
+                grade: getTableElement(table, "grade"),
+                stories: getTableElement(table, "stories"),
+                heatType: getTableElement(table, "heat type", "heating type"),
+                heatFuel: getTableElement(table, "heat fuel", "heating fuel"),
+                address: `${document.getElementById("MainContent_lblLocation").innerText} ${document.getElementById("lblTownName").innerText}`,
             };
         });
         const house = new House(houseData);
         house.save(function (err) {
-            if (err) return console.log(err);
+            if (err) {
+                console.log(err);
+            };
             return console.log("Housing Data saved!");
         });
     };
 
-
-    const visitStreet = async ({ page, data: url }) => {
-        console.log(`Going to ${url}`);
-        await page.goto(url);
-        const letterLinks = await page.evaluate(() => {
-            const data = Array.from(document
-                .querySelectorAll(".buttonMe > a"))
-                .filter(link => link.querySelector("span").innerText != "-")
-                .map(link => link.href);
-            return data;
-        });
-        console.log(letterLinks);
-        for(const letterLink of letterLinks) {
-            await page.goto(letterLink);
-            const streetLinks = await page.evaluate(() => {
-                return Array.from(document.querySelectorAll("#list > li > a")).map(link => link.href);
-            });
-
-            for(const streetLink of streetLinks) {
-                await page.goto(streetLink);
-                const homeLinks = await page.evaluate(() => {
-                    return Array.from(document.querySelectorAll("#list > li > a")).map(link => link.href);
-                });
-                for(const home of homeLinks){
-                    console.log(`Queueing home at ${home}`);
-                    await cluster.queue(home, saveHouse);
-                }
-            }
-        }
-    };
-
-    mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+    mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true, useUnifiedTopology: true});
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, 'connection error:'));
     db.once('open', function() {
         console.log('open database');
     });
 
-    cluster.queue(async ({page}) => {
-        await page.goto('https://www.vgsi.com/massachusetts-online-database/');
-        let links = await page.evaluate(() => {
-            return Array.from(document.querySelectorAll("tbody > tr > td > a")).map(link => link.href);
-        });
-        for (const link of links) {
-            const linkToStreets = link.endsWith('/') ? `${link}Streets.aspx` : `${link}/Streets.aspx`;
-            await cluster.queue(linkToStreets, visitStreet);
-            break;
-        }
-    })
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+
+    await page.goto('https://www.vgsi.com/massachusetts-online-database/');
+    let links = await page.evaluate(() => {
+        return Array.from(document.querySelectorAll("tbody > tr > td > a")).map(link => link.href);
+    });
+
+
+     for (const link of links) {
+         const linkToStreets = link.endsWith('/') ? `${link}Streets.aspx` : `${link}/Streets.aspx`;
+
+         await page.goto(linkToStreets);
+         const letterLinks = await page.evaluate(() => {
+             const data = Array.from(document
+                 .querySelectorAll(".buttonMe > a"))
+                 .filter(link => link.querySelector("span").innerText != "-")
+                 .map(link => link.href);
+             return data;
+         });
+
+         console.log(letterLinks);
+         for(const letterLink of letterLinks) {
+             await page.goto(letterLink);
+             const streetLinks = await page.evaluate(() => {
+                 return Array.from(document.querySelectorAll("#list > li > a")).map(link => link.href);
+             });
+             for(const streetLink of streetLinks) {
+                 await page.goto(streetLink);
+                 const homeLinks = await page.evaluate(() => {
+                     return Array.from(document.querySelectorAll("#list > li > a")).map(link => link.href);
+                 });
+                 for(const home of homeLinks){
+                     await cluster.queue(home, saveHouse);
+                 }
+             }
+         }
+         break;
+     }
 
     await cluster.idle();
+    await browser.close();
     await cluster.close();
 })();
