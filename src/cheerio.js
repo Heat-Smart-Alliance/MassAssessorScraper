d5ed6d4791ac1fe43ecf205f57c1683f4f68449b
dodellmitchell@gmail.com <dodellmitchell@gmail.com>
2020-12-23 12:48:55 -0800

    Set up MongoDB

--- src/cheerio.js
+++ src/cheerio.js
@@ -1,6 +1,10 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+
 const PromisePool = require('@supercharge/promise-pool');
+
+const mongoose = require('mongoose');
+const House = require('../models/House');
 
 Object.defineProperty(Array.prototype, 'flat', {
     value: function (depth = 1) {
@@ -19,25 +23,31 @@ Object.defineProperty(Array.prototype, 'chunk', {
     }
 });
 
-(async function () {
-    try {
-        const base = 'https://www.vgsi.com/massachusetts-online-database/';
-        const {data} = await axios.get(base);
-        const $ = cheerio.load(data);
+async function scrapeHouses() {
+    const db = mongoose.connection;
+    const base = 'https://www.vgsi.com/massachusetts-online-database/';
+    const {data} = await axios.get(base);
+    const $ = cheerio.load(data);
 
-        const townLinks = $('.bluelink').map((i, elem) => {
-            const link = elem.attribs.href;
-            const townLink = link.endsWith('/') ? link.slice(0, -1) : `${link}`;
-            const streetsSuffix = 'Streets.aspx';
-            return {
-                townLink: `${townLink}/${streetsSuffix}`,
-                baseLink: townLink
-            };
-        }).get();
+    let townLinks = $('.bluelink').map((i, elem) => {
+        const link = elem.attribs.href;
+        const townLink = link.endsWith('/') ? link.slice(0, -1) : `${link}`;
+        const streetsSuffix = 'Streets.aspx';
+        return {
+            townLink: `${townLink}/${streetsSuffix}`,
+            baseLink: townLink
+        };
+    }).get();
 
-        let letterLinks = await Promise.all(townLinks.map(async ({townLink, baseLink}) => {
-            try {
-                const {data} = await axios.get(townLink);
+    townLinks = townLinks.slice(0, 1).chunk(1);
+    console.log(townLinks);
+
+    let townPromises = townLinks.map(async (townLinks) => {
+        const { results, errors } = await PromisePool
+            .for(townLinks)
+            .withConcurrency(3)
+            .process(async ({townLink, baseLink}) => {
+                const { data } = await axios.get(townLink);
                 const $ = cheerio.load(data);
                 const links = $('div.buttonMe a').map((i, a) => {
                     return {
@@ -46,15 +56,21 @@ Object.defineProperty(Array.prototype, 'chunk', {
                     }
                 }).get();
                 return links;
-            } catch (e) {
-                console.log("letter link error", townLink, e);
-            }
-        }));
+            });
+        return results;
+    });
 
-        letterLinks = letterLinks.filter(link => link).flat();
+    let letterLinks = await Promise.all(townPromises);
+    letterLinks = letterLinks.flat();
+    console.log(letterLinks);
 
-        let streetLinks = await Promise.all(letterLinks.map(async ({baseLink, letterLink}) => {
-            try {
+    let streetPromises = letterLinks.map(async letterLinks => {
+        const { results, errors } = await PromisePool
+            .for(letterLinks)
+            .withConcurrency(5)
+            .handleError(async (error, data) => {
+                console.log("Street Promises error:", data, error);
+            }).process(async ({letterLink, baseLink}) => {
                 const {data} = await axios.get(letterLink);
                 const $ = cheerio.load(data);
                 const links = $('li.fixedButton a').map((i, a) => {
@@ -64,54 +80,76 @@ Object.defineProperty(Array.prototype, 'chunk', {
                     };
                 }).get();
                 return links;
-            } catch (e) {
-                console.log('street link error', letterLink, e)
-            }
-        }));
+            })
+        return results;
+    });
 
-        streetLinks = streetLinks.filter(link => link).flat().chunk(500);
+    let streetLinks = await Promise.all(streetPromises);
 
-        console.log("Chunk Length", streetLinks.length);
-        let houseLinks = [];
-        let chunk = 0;
-        for(let streets of streetLinks) {
-            console.log(`On chunk: ${++chunk}`);
-            const {results, errors} = await PromisePool
-                .for(streets)
-                .withConcurrency(500)
-                .handleError(async (error, user) => {
-                    console.log("Housing links error", error);
-                })
-                .process(async ({baseLink, streetLink}) => {
-                    const {data} = await axios.get(streetLink);
-                    const $ = cheerio.load(data);
-                    const links = $('#list > li > a').map((i, a) => {
-                        return `${baseLink}/${$(a).attr('href')}`;
-                    }).get();
-                    return links;
-                });
-            houseLinks.push(results);
-            console.log(results);
-            console.log(errors);
-        }
+    // TODO: Remove slicing in order to not limit the number of houses
+    streetLinks = streetLinks.flat(2);
+    console.log(streetLinks);
+    let housePromises = streetLinks.chunk(10).map(async streets => {
+        const { results, errors } = await PromisePool
+            .for(streets)
+            .withConcurrency(3)
+            .handleError(async error => {
+                console.log(`House Promises Error: ${error}`);
+            })
+            .process(async ({baseLink, streetLink}) => {
+                const { data } = await axios.get(streetLink);
+                const $ = cheerio.load(data);
+                const links = $('#list > li > a').map((i, a) => {
+                    return `${baseLink}/${$(a).attr('href')}`
+                }).get();
+                console.log(links);
+                return links;
+            })
+        return results;
+    });
 
-        houseLinks = houseLinks.flat(2).chunk(10000);
-        let houseChunk = 0;
-        for(let houses of houseLinks){
-            console.log(`On house chunk ${++houseChunk} of ${houseLinks.length}`);
-            const { result, errors } = await PromisePool
-                .for(houses)
-                .withConcurrency(1000)
-                .handleError(async (error, user) => {
-                    console.log("Housing links error", error);
-                })
-                .process(async link => {
-                   const { data } = await axios.get(link);
-                   const $ = cheerio.load(data);
-                   
-                });
-        }
-        console.log("House links are now flattened", houseLinks);
+    let houseLinks = await Promise.all(housePromises);
+    houseLinks = houseLinks.flat(2).chunk(100);
+    console.log(houseLinks);
+
+    let databasePromises = houseLinks.map(async houses => {
+        const { results, errors } = await PromisePool
+            .for(houses)
+            .withConcurrency(2)
+            .handleError(async error => {
+                console.log(`Error saving to the database:`, error);
+            })
+            .process(async houseLink => {
+                const { data } = await axios.get(houseLink);
+                const $ = cheerio.load(data);
+                console.log("House Link: ", houseLink);
+                const houseData = {
+                    address: `${$("#MainContent_lblLocation").text()} ${$("#lblTownName").text()}`
+                };
+                return houseData;
+            });
+        return results;
+    });
+
+    let houses = await Promise.all(databasePromises);
+    console.log(houses);
+    db.close();
+}
+
+(async function () {
+    try {
+        const DATABASE_NAME = "test";
+        const DATABASE_URL = `mongodb://localhost:27017/${DATABASE_NAME}`;
+        await mongoose.connect(DATABASE_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(() => {
+            console.log("Connecting! Starting to scrape houses now!");
+            scrapeHouses();
+        }).catch(e => console.log(e));
+        const db = mongoose.connection;
+
+
 
     } catch (e) {
         console.log('Bad Error', e);
